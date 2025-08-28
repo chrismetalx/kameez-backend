cc41739a194f6b4f59ae72a7963b9fd324f64cc8
unknown <christpherjesus@gmail.com>
2025-08-28 16:17:38 -0400

    user routes

--- src/database.js
+++ src/database.js
@@ -3,7 +3,7 @@ const { DB_DATABASE, DB_HOST, DB_USER, DB_PASSWORD } = require('./config.js');
 module.exports = {
   development: {
     username: DB_USER,
-    password: '',
+    password: DB_PASSWORD,
     database: DB_DATABASE,
     host: DB_HOST,
     dialect: "mysql"
